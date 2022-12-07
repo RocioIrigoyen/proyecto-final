90355606694554075f4d62ba4085de8a28c6ad6b
Rocío Irigoyen :) <rocioirigoyen94@gmail.com>
2022-12-07 20:12:09 -0300

    Agrego vista,url y template para crear familiar

--- ejemplo/models.py
+++ ejemplo/models.py
@@ -4,6 +4,8 @@ class Familiar(models.Model):  #HERENCIA de una clase existente, Model
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     numero_pasaporte = models.IntegerField()
+    #creado_el= models.DateTimeField()
+    #modificado_el= models.DateTimeField()
     
     def __str__(self):
       return f"{self.nombre}, {self.numero_pasaporte}, {self.id}"
